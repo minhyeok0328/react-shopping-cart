017ccc98096cadffd069063cc319fe36ac635976
김민혁 <alsgur7408@gmail.com>
2023-03-21 15:23:13 +0900

    feat: http 요청 에러 핸들링 추가 axios interceptor로 에러 발생하면 console.error 출력 및 빈 배열 리턴하도록 변경

--- src/client/httpClient.ts
+++ src/client/httpClient.ts
@@ -4,3 +4,11 @@ export const http = axios.create({
   baseURL: '/',
   timeout: 5000
 });
+
+http.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    console.error(error);
+    return [];
+  }
+);
